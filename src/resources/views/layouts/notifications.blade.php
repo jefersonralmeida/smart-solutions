415048a25182d4644f78c9b1aa2bf12f6f25d696
Jeferson Almeida <jeferson.almeida@codelathe.com>
2019-09-25 22:06:06 -0300

    Ticket: https://github.com/jefersonralmeida/smart-solutions/issues/14

--- src/resources/views/layouts/notifications.blade.php
+++ src/resources/views/layouts/notifications.blade.php
@@ -4,26 +4,23 @@
         <em class="fa fa-bell"></em>
         <span id="notifications-counter" class="label label-info"></span>
     </a>
-    <ul id="notifications-list" class="dropdown-menu dropdown-alerts" style="width: 500px;">
-
-    </ul>
-    {{--@if ($notificationCount)--}}
-        {{--<ul class="dropdown-menu dropdown-alerts" style="width: 500px;">--}}
-            {{--@foreach (Auth::user()->unreadNotifications as $notification )--}}
-                {{--<li>--}}
-                    {{--<a href="{{ route('notifications.read', ['notification' => $notification->id]) }}">--}}
-                        {{--<div style="margin-bottom: 8px;">--}}
-                            {{--<strong>{{ $notification->data['title'] }}</strong>--}}
-                            {{--<span class="pull-right text-muted small">{{ $notification->created_at->format('d/m/Y H:i') }}</span>--}}
-                        {{--</div>--}}
-                        {{--@foreach ($notification->data['message'] as $line)--}}
-                            {{--<p>{{ $line }}</p>--}}
-                        {{--@endforeach--}}
-                    {{--</a>--}}
-                {{--</li>--}}
-            {{--@endforeach--}}
-        {{--</ul>--}}
-    {{--@endif--}}
+{{--    @if ($notificationCount)--}}
+{{--        <ul class="dropdown-menu dropdown-alerts" style="width: 500px;">--}}
+{{--            @foreach (Auth::user()->unreadNotifications as $notification )--}}
+{{--                <li>--}}
+{{--                    <a href="{{ route('notifications.read', ['notification' => $notification->id]) }}">--}}
+{{--                        <div style="margin-bottom: 8px;">--}}
+{{--                            <strong>{{ $notification->data['title'] }}</strong>--}}
+{{--                            <span class="pull-right text-muted small">{{ $notification->created_at->format('d/m/Y H:i') }}</span>--}}
+{{--                        </div>--}}
+{{--                        @foreach ($notification->data['message'] as $line)--}}
+{{--                            <p>{{ $line }}</p>--}}
+{{--                        @endforeach--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--            @endforeach--}}
+{{--        </ul>--}}
+{{--    @endif--}}
 </li>
 
 <script language="javascript">
