ee5a3636eb0e33ab2fddce83d4a51f581621507b
Jeferson Almeida <jeferson.almeida@codelathe.com>
2019-10-29 23:41:26 -0300

    Google login

--- src/config/services.php
+++ src/config/services.php
@@ -43,13 +43,13 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => env('FACEBOOK_CALLBACK_URL'),
+        'redirect' => config('app.url') . '/login/facebook/callback',
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_CALLBACK_URL'),
+        'redirect' => config('app.url') . '/login/google/callback',
     ],
 
 ];
